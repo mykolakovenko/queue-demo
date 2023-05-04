6190c72317081816560349be0a4940eda63c3389
mkovenko <mkovenko@shutterstock.com>
2023-05-04 11:30:11 +0200

    Added signature

--- pdf-service.js
+++ pdf-service.js
@@ -1,7 +1,15 @@
 import os from 'os';
+import crypto from 'crypto';
 import { jsPDF } from 'jspdf';
 import { getBase64OfRemoteFile } from './file-service.js';
 import { uploadFileToS3 } from './s3-service.js';
+
+
+const generateSignature = () => new Promise(resolve => {
+  crypto.pbkdf2(new Date().getTime().toString(), 'salt', 100000, 512, 'sha512', (error, value) => {
+    resolve(value.toString('hex'));
+  });
+});
 
 export const generatePdf = async (user) => {
   const doc = new jsPDF();
@@ -17,6 +25,9 @@ export const generatePdf = async (user) => {
 
   const image = await getBase64OfRemoteFile(user.avatarUrl);
   doc.addImage(image, 'JPEG', 10, 60, 160, 120);
+
+  const signature = await generateSignature();
+  doc.text(`Signature: ${signature}`, 10, 270);
 
   const localFilePath = `${os.tmpdir()}/${user.id}.pdf`;
   await doc.save(localFilePath);
