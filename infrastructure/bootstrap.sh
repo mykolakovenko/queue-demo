1e1d34757508af5b56a7255ef78817c465793cae
mkovenko <mkovenko@shutterstock.com>
2023-05-08 08:26:29 +0200

    Added bootstrap script

---
+++ infrastructure/bootstrap.sh
@@ -0,0 +1,8 @@
+sudo yum update
+sudo yum install -y git
+sudo yum install -y nodejs
+
+git clone https://github.com/mykolakovenko/queue-demo.git
+cd queue-demo/
+npm install
+npm start > app.log 2>&1 &
