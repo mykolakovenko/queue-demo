eed46b33abe90005301a2982f0cc8858d6650e23
mkovenko <mkovenko@shutterstock.com>
2023-05-11 09:06:29 +0200

    Load tests

---
+++ load-tests/load-config.js
@@ -0,0 +1,4 @@
+export const options = {
+  vus: 10,
+  duration: '30s',
+};
--- load-tests/producer-instance.js
+++ load-tests/producer-instance.js
@@ -4,6 +4,6 @@ import { sleep } from 'k6';
 export { options } from './load-config.js';
 
 export default function () {
-  http.post('http://queue-demo-single-instance.mkovenko.rocks:8080/user');
+  http.post('http://queue-demo-producer-instance.mkovenko.rocks:8080/user');
   sleep(1);
 }
